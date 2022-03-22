9a8c542819a3ee9b96b619c1d434fed72c17b50b
Bulat <57670154+MadPuffer@users.noreply.github.com>
2022-03-22 16:04:18 +0300

    EventHandler

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace BarsEdu
+namespace practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            EventExample ex = new EventExample();
+            ex.OnKeyPressed += (_, c) => Console.WriteLine($"\n{c}");
+            
+            ex.Run();
+        }
+    }
+    
+    class EventExample
+    {
+        public EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            char key;
+            while ((key = Console.ReadKey().KeyChar) != 'c')
+            {
+                OnKeyPressed?.Invoke(this, key);
+            }
         }
     }
 }
+
