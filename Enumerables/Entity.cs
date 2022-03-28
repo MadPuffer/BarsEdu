f47b47b76165b5d62da1f41b098221c9b9b74799
Bulat <57670154+MadPuffer@users.noreply.github.com>
2022-03-28 21:14:38 +0300

    Add files via upload

---
+++ Enumerables/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace BarsEdu
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Enumerables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BarsEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Entity> entities = new List<Entity>
+            {
+                new Entity { Id = 1, ParentId = 0, Name = "Root entity"},
+                new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"},
+                new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"},
+            };
+            
+            var dict = GroupByParents(entities);
+        }
+
+        static Dictionary<int, List<Entity>> GroupByParents(List<Entity> entities)
+        {
+            var dict = new Dictionary<int, List<Entity>>();
+
+            foreach (var entity in entities)
+            {
+                dict.TryAdd(entity.ParentId, new List<Entity>());
+                dict[entity.ParentId].Add(entity);
+            }
+            
+            return dict;
+        }
+    }
+}
