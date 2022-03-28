0e085559f7bd3c1505388d230634fff91084c4c6
Bulat <57670154+MadPuffer@users.noreply.github.com>
2022-03-28 20:21:08 +0300

    Events task

---
+++ Events/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventExample ex = new EventExample();
+            ex.OnKeyPressed += (_, c) => Console.WriteLine($"\n{c}");
+            
+            ex.Run();
+        }
+    }
+    
+    class EventExample
+    {
+        public EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            char key;
+            while ((key = Console.ReadKey().KeyChar) != 'c')
+            {
+                OnKeyPressed?.Invoke(this, key);
+            }
+        }
+    }
+}
+
